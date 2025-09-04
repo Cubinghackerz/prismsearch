20f9a8d53598877e6b6a9cb068e59a7211058db1
PawnStormer <pawnstormer10101@gmail.com>
2025-09-04 19:41:51 +0800

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -236,7 +236,7 @@ const Home = () => {
                       Try Demo
                       <ArrowRight className="h-4 w-4 ml-2" />
                     </Link>
-                {stat.value}
+                  </Button>
                 </div>
               </div>
             </div>
