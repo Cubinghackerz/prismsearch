bc86f3bb16d106eb2232a70fc04f537110a50ac4
PawnStormer <pawnstormer10101@gmail.com>
2025-09-04 19:41:43 +0800

    Updated Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -179,7 +179,7 @@ const Index = () => {
                 <div className="text-4xl font-bold text-primary mb-2 font-fira-code">
                   <CountUp to={stat.number} />
                   {stat.suffix}
-                {stat.number}
+                </div>
                 <div className="text-muted-foreground font-medium">{stat.label}</div>
               </div>
             ))}
